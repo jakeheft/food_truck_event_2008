1597a7b19916a5b64a3047f8e4daa9cd15ef2b06
Jake Heft <jakeheft@gmail.com>
2020-09-15 13:14:19 -0600

    Add test it exists, passes

--- test/food_truck.test.rb
+++ test/food_truck.test.rb
@@ -4,7 +4,9 @@ require './lib/item'
 require './lib/food_truck'
 
 class FoodTruckTest < Minitest::Test
-  food_truck = FoodTruck.new("Rocky Mountain Pies")
+  def test_it_exists
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
 
-  assert_instance_of FoodTruck, food_truck
+    assert_instance_of FoodTruck, food_truck
+  end
 end
