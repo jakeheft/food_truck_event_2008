b43a6135c90d4da24a66335e6eccd8c3c5ab43dc
Jake Heft <jakeheft@gmail.com>
2020-09-15 13:33:14 -0600

    Add attr_reader

--- test/event_test.rb
+++ test/event_test.rb
@@ -10,4 +10,10 @@ class EventTest < Minitest::Test
 
     assert_instance_of Event, event
   end
+
+  def test_it_has_attributes
+    event = Event.new("South Pearl Street Farmers Market")
+
+    assert_equal "South Pearl Street Farmers Market", event.name
+  end
 end
